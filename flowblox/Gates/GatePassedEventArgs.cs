55278790ddf13ecb5752581eb03a871032ea3aa3
Uwe Mayer <mayer@software-driven.de>
2014-01-23 16:00:57 +0100

    Gates

---
+++ flowblox/Gates/GatePassedEventArgs.cs
@@ -0,0 +1,27 @@
+﻿namespace flowblox.Gates
+{
+    using flowblox.Data;
+    using System;
+
+    /// <summary>
+    /// This class is used for gate-pass notifications
+    /// </summary>
+    /// <typeparam name="T">The type of the payload.</typeparam>
+    public class GatePassedEventArgs<T> : EventArgs
+        where T : class
+    {
+        /// <summary>
+        /// The container that passed the gate.
+        /// </summary>
+        public Container<T> Container { get; private set; }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="GatePassedEventArgs"/>.
+        /// </summary>
+        /// <param name="container">The container that passed gate.</param>
+        public GatePassedEventArgs(Container<T> container)
+        {
+            this.Container = container;
+        }
+    }
+}
---
+++ flowblox/Gates/IGate.cs
@@ -0,0 +1,37 @@
+﻿namespace flowblox.Gates
+{
+    using flowblox.Data;
+    using System;
+
+    /// <summary>
+    /// An interface for gates.
+    /// </summary>
+    public interface IGate
+    {
+        /// <summary>
+        /// Clears the inbound queue.
+        /// </summary>
+        void ClearInboundQueue();
+    }
+
+    public interface IGate<T> : IGate
+        where T : class
+    {
+        /// <summary>
+        /// Gets the next inbound container.
+        /// </summary>
+        /// <returns>The next container in the inbound queue.</returns>
+        Container<T> GetNextInboundContainer();
+
+        /// <summary>
+        /// Sends a new container.
+        /// </summary>
+        /// <param name="container">The container to send.</param>
+        void SendContainer(Container<T> container);
+
+        /// <summary>
+        /// Gets or sets the action for inbound container reception.
+        /// </summary>
+        Action<Container<T>> AddInboundContainer { get; set; }
+    }
+}
