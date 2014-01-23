f6a5a1f5c0d07e0e603da3ad3fe7e1d6c269dbba
Uwe Mayer <mayer@software-driven.de>
2014-01-23 14:46:14 +0100

    Added route cards and containers

---
+++ flowblox/Data/Container.cs
@@ -0,0 +1,76 @@
+﻿namespace flowblox.Data
+{
+    using System.Collections.ObjectModel;
+    using System.ComponentModel;
+
+    /// <summary>
+    /// A container base class.
+    /// </summary>
+    public abstract class Container : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged Implementation
+        /// <summary>
+        /// Raised when a property changed.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Raises the <see cref="E:INotifyPropertyChanged.PropertyChanged"/> event.
+        /// </summary>
+        /// <param name="propertyName">The name of the property.</param>
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+        private ObservableCollection<RouteCardEntry> routeCard = null;
+        /// <summary>
+        /// Gets all entries on the route card.
+        /// </summary>
+        public ObservableCollection<RouteCardEntry> RouteCard
+        {
+            get
+            {
+                if (routeCard == null)
+                    routeCard = new ObservableCollection<RouteCardEntry>();
+                return routeCard;
+            }
+        }
+
+        /// <summary>
+        /// Hands over an existing route card to this instance.
+        /// </summary>
+        /// <param name="routeCard">The existing route card.</param>
+        public virtual void HandoverRouteCard(ObservableCollection<RouteCardEntry> routeCard)
+        {
+            this.routeCard = routeCard;
+        }
+    }
+
+    /// <summary>
+    /// A typed base class for containers.
+    /// </summary>
+    /// <typeparam name="T">The payload class type.</typeparam>
+    public class Container<T> : Container
+        where T : class
+    {
+        private T payload = null;
+        /// <summary>
+        /// Gets or sets the payload.
+        /// </summary>
+        public T Payload
+        {
+            get { return payload; }
+            set
+            {
+                if (payload != value)
+                {
+                    payload = value;
+                    RaisePropertyChanged("Payload");
+                }
+            }
+        }
+    }
+}
---
+++ flowblox/Data/RouteCardEntry.cs
@@ -0,0 +1,41 @@
+﻿namespace flowblox.Data
+{
+    using System;
+    using System.Xml.Serialization;
+
+    /// <summary>
+    /// This class encapsulates entries for the data route card.
+    /// </summary>
+    public class RouteCardEntry
+    {
+        /// <summary>
+        /// The time stamp, when the data arrived at the port.
+        /// </summary>
+        [XmlAttribute]
+        public DateTime TimeStamp { get; set;}
+
+        /// <summary>
+        /// The processing port's GuID.
+        /// </summary>
+        [XmlAttribute]
+        public Guid ProcessingPort { get; set; }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="RouteCardEntry"/>.
+        /// </summary>
+        /// <param name="timeStamp">The time stamp, when the data arrived at the port.</param>
+        /// <param name="processingPort">The processing port's GuID.</param>
+        public RouteCardEntry(DateTime timeStamp, Guid processingPort)
+        {
+            this.TimeStamp = timeStamp;
+            this.ProcessingPort = processingPort;
+        }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="RouteCardEntry"/>.
+        /// </summary>
+        public RouteCardEntry()
+        {
+        }
+    }
+}
